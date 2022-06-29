c4f3d029eb520ef9ddee1b13e698f6e921c4e6e6
Nikhil Shahi <shniks@gmail.com>
2022-06-29 13:08:05 -0700

    Created test and method to write more than one book and populate the books Array. Completed iteration 2.

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,29 @@
 require './lib/book'
+require 'pry'
 
 class Author
 
-  attr_reader :name,
+  attr_reader :first_name,
+              :last_name,
               :books
 
   def initialize(author)
-    @name = "#{author[:first_name]} #{author[:last_name]}"
+    @first_name = author[:first_name]
+    @last_name = author[:last_name]
     @books = []
   end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def write(title, publication_date)
+    book = Book.new({author_first_name: first_name, author_last_name: last_name,
+                      title: title, publication_date: publication_date})
+    books << book
+    book 
+  end
+
 
 
 
--- test/author_test.rb
+++ test/author_test.rb
@@ -20,6 +20,25 @@ class AuthorTest < Minitest::Test
     assert_instance_of Array, charlotte_bronte.books
   end
 
+  def test_if_it_can_write_a_book
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, jane_eyre
+    assert_equal "Jane Eyre", jane_eyre.title
+  end
+
+  def test_if_it_can_write_another_book
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    assert_instance_of Book, jane_eyre
+    assert_instance_of Book, villette
+    assert_equal "Jane Eyre", jane_eyre.title
+    assert_equal 2, charlotte_bronte.books.count
+  end
+
 
 
 
