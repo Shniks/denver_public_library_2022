13e864f121166a7e64c1f7efa12f20b7b46166d0
Nikhil Shahi <shniks@gmail.com>
2022-06-29 11:37:24 -0700

    Created tests and methods to check existence and attributes. Completed iteration 1.

---
+++ test/book_test.rb
@@ -0,0 +1,28 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/book'
+
+class BookTest < Minitest::Test
+
+  def test_if_it_exists
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_instance_of Book, book
+  end
+
+  def test_if_it_has_attributes
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_equal "To Kill a Mockingbird", book.title
+    assert_equal "Harper Lee", book.author
+    assert_equal "1960", book.publication_year
+  end
+
+
+
+
+
+end
