6cdef744a8dde5a01de3bc9fc0b0dffd921529e0
Nikhil Shahi <shniks@gmail.com>
2022-06-29 15:56:45 -0700

    Created test to add authors to Library class.

--- test/library_test.rb
+++ test/library_test.rb
@@ -21,6 +21,21 @@ class LibraryTest < Minitest::Test
     assert_equal [], dpl.authors
   end
 
+  def test_if_it_has_authors
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    assert_equal [charlotte_bronte, harper_lee], dpl.authors 
+  end
+
 
 
 
