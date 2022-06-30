367932a0ea893b016e8ffbba5fa1db1baff9bbb4
Nikhil Shahi <shniks@gmail.com>
2022-06-30 10:42:28 -0700

    Created test for most popular book.

--- test/library_test.rb
+++ test/library_test.rb
@@ -81,11 +81,29 @@ class LibraryTest < Minitest::Test
 
     assert dpl.checkout(jane_eyre)
     assert_equal [jane_eyre], dpl.checked_out_books
-
     refute dpl.checkout(jane_eyre) #This book cannot be checked out as it is alrleady checked out
   end
 
   def test_if_a_book_can_be_returned
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.checkout(jane_eyre)
+    dpl.return(jane_eyre)
+
+    assert_equal [], dpl.checked_out_books
+    assert dpl.checkout(jane_eyre)
+    assert dpl.checkout(villette)
+    assert_equal [jane_eyre, villette], dpl.checked_out_books
+  end
+
+  def test_if_a_book_is_most_popular
     dpl = Library.new("Denver Public Library")
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
@@ -97,11 +115,18 @@ class LibraryTest < Minitest::Test
     dpl.add_author(harper_lee)
     dpl.checkout(jane_eyre)
     dpl.return(jane_eyre)
+    dpl.checkout(jane_eyre)
+    dpl.checkout(villette)
 
-    assert_equal [], dpl.checked_out_books
-    assert dpl.checkout(jane_eyre)
-    assert dpl.checkout(villette)
     assert_equal [jane_eyre, villette], dpl.checked_out_books
+
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+
+    assert_equal mockingbird, dpl.most_popular_book
   end
 
 end
